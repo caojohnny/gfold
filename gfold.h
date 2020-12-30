830d4bc68a39d4879828f6df6123e3400ae992bb
AgentTroll <5037004+AgentTroll@users.noreply.github.com>
2020-12-30 03:52:31 -0800

    Code cleanup

---
+++ gfold.h
@@ -0,0 +1,263 @@
+#ifndef GFOLD_GFOLD_H
+#define GFOLD_GFOLD_H
+
+#include <Eigen/Dense>
+#include <epigraph.hpp>
+
+namespace gfold {
+    /**
+     * @brief Properties of the lander used to compute the
+     * optimal trajectory.
+     */
+    struct gfold_lander_data {
+        /**
+         * The dry mass of the lander, kilograms.
+         */
+        double m_0;
+        /**
+         * The initial mass of the fuel on the lander,
+         * kilograms.
+         */
+        double m_f;
+        /**
+         * The landing propulsion system's minimum throttle,
+         * Newtons.
+         */
+        double rho_1;
+        /**
+         * The landing propulsion system's maximum throttle,
+         * Newtons.
+         */
+        double rho_2;
+        /**
+         * The mass depletion constant (the amount of fuel
+         * depleted dependent on the thrust produced),
+         * seconds/meter.
+         */
+        double alpha;
+        /**
+         * The maximum velocity the lander is permitted to
+         * experience, meters/second.
+         */
+        double V_max;
+    };
+
+    /**
+     * @brief Properties of the computed optimal trajectory.
+     */
+    struct gfold_trajectory_data {
+        /**
+         * The initial position of the trajectory, meters.
+         */
+        Eigen::Vector3d r_0;
+        /**
+         * The initial velocity vector, meters/second.
+         */
+        Eigen::Vector3d r_dot_0;
+        /**
+         * The glideslope angle (measured from the ground to
+         * the position of the lander), radians.
+         */
+        double gamma_gs;
+        /**
+         * The maximum deviation from the thrust pointing angle
+         * (measured from the thrust unit vector), radians.
+         */
+        double theta;
+        /**
+         * The thrust unit vector.
+         */
+        Eigen::Vector3d n_hat;
+        /**
+         * The position of the desired landing site, meters.
+         */
+        Eigen::Vector2d q;
+    };
+
+    /**
+     * @brief Properties of the landing surface.
+     */
+    struct gfold_planet_data {
+        /**
+         * The gravitational constant of the body with the
+         * landing surface, meters/second^2.
+         */
+        Eigen::Vector3d g;
+        /**
+         * The angular velocity of the body's surface,
+         * radians/second.
+         */
+        Eigen::Vector3d omega;
+    };
+
+    /**
+     * @brief Wrapper class for the G-FOLD algorithm,
+     * containing the input and output parameters and the state
+     * of the conic solver.
+     */
+    class gfold {
+    private:
+        const struct gfold_lander_data lander_data;
+        const struct gfold_trajectory_data trajectory_data;
+        const struct gfold_planet_data planet_data;
+
+        const double min_duration;
+        const double max_duration;
+        const double duration_search_interval;
+        const double dt;
+
+        struct gfold_state {
+            int i_f;
+
+            Eigen::MatrixXd x;
+            Eigen::MatrixXd m;
+            Eigen::MatrixXd sigma;
+            Eigen::MatrixXd u;
+            Eigen::MatrixXd z;
+        } socp_state;
+
+        /**
+         * Performs the computation for Problem 3 and then
+         * Problem 4 if the given duration is sufficient to
+         * find an optimal solution.
+         *
+         * @param duration the desired duration
+         * @return true if an optimal solution was found
+         */
+        bool p3p4(double duration);
+
+    public:
+        /**
+         * Configures a new instance of the G-FOLD
+         * algorithm with the given parameters. This
+         * constructor will optimize for the time of
+         * flight, which will be returned from the
+         * algorithm through get_time_steps() in the
+         * specified range.
+         *
+         * @param lander_data the lander parameters
+         * @param trajectory_data the trajectory parameters
+         * @param planet_data the target data
+         * @param min_duration the minimum duration to
+         * begin looking for an optimal solution
+         * @param max_duration the maximum duration to
+         * constrain the solution
+         * @param duration_search_interval the interval
+         * between consecutive flight durations which to
+         * look for an optimal trajectory
+         * @param dt the time step for the solution
+         */
+        gfold(gfold_lander_data lander_data,
+              gfold_trajectory_data trajectory_data,
+              gfold_planet_data planet_data,
+              double min_duration,
+              double max_duration,
+              double duration_search_interval,
+              double dt);
+
+        /**
+         * Configures a new instance of the G-FOLD
+         * algorithm with the given parameters. This
+         * constructor affixes the possible duration search
+         * at the given duration. All members of the
+         * parameter structs must be filled.
+         *
+         * @param lander_data the lander parameters
+         * @param trajectory_data the trajectory parameters
+         * @param planet_data the target data
+         * @param duration the duration until landing
+         * @param dt the time step for the solution
+         */
+        gfold(gfold_lander_data lander_data,
+              gfold_trajectory_data trajectory_data,
+              gfold_planet_data planet_data,
+              double duration,
+              double dt);
+
+        /**
+         * Computes the optimization problem and determines
+         * if a feasible solution was found.
+         *
+         * @return true if a feasible solution was found
+         */
+        bool compute();
+
+        /**
+         * Obtains the number of time steps needed to find
+         * an optimal trajectory from the compute() method.
+         *
+         * Not valid if compute() does not return true.
+         *
+         * @return the number of time steps for the optimal
+         * trajectory
+         */
+        int get_time_steps() const;
+
+        /**
+         * Returns a 6xN matrix in which each column is the
+         * state vector at t = j*dt. A state vector is an
+         * X, Y, Z, X_dot, Y_dot and Z_dot or the position
+         * and velocity in meters and meters/second,
+         * respectively.
+         *
+         * N is given by get_time_steps().
+         *
+         * Not valid if compute() does not return true.
+         *
+         * @return the state vectors at each discrete time
+         * instant
+         */
+        const Eigen::MatrixXd &get_state_vector() const;
+
+        /**
+         * Obtains the 1xN mass matrix containing the
+         * lander mass at t = j*dt.
+         *
+         * N is given by get_time_steps().
+         *
+         * Not valid if compute() does not return true.
+         *
+         * @return the mass matrix
+         */
+        const Eigen::MatrixXd &get_mass() const;
+
+        /**
+         * Obtains the thrust slack variable containing the
+         * slack divided by mass at t = j*dt.
+         *
+         * N is given by get_time_steps().
+         *
+         * Not valid if compute() does not return true.
+         *
+         * @return the thrust slack matrix
+         */
+        const Eigen::MatrixXd &get_sigma() const;
+
+        /**
+         * Obtains the 3xN thrust matrix containing the
+         * thrust divided by mass value at t = j*dt.
+         *
+         * N is given by get_time_steps().
+         *
+         * Not valid if compute() does not return true.
+         *
+         * @return the thrust matrix
+         */
+        const Eigen::MatrixXd &get_u() const;
+
+        /**
+         * Obtains the 1xN mass matrix containing the
+         * natrual logarithm of the lander's mass at
+         * t = j*dt.
+         *
+         * N is given by get_time_steps().
+         *
+         * Not valid if compute() does not return true.
+         *
+         * @return the mass matrix
+         */
+        const Eigen::MatrixXd &get_z() const;
+    };
+}
+
+#endif // GFOLD_GFOLD_H
--- main.cpp
+++ main.cpp
@@ -1,143 +1,48 @@
-#include <iostream>
-
 #include <epigraph.hpp>
 #include <mpl/matplotlibcpp.h>
+
+#include "gfold.h"
 
 namespace mpl = matplotlibcpp;
 
 int main() {
-    double m_0 = 2000.0; // Initial mass, kg
-    double m_f = 300.0; // Fuel mass, kg
-    double T_max = 24000.0; // Maximum thrust, N
-    double rho_1 = 0.2 * T_max; // Thrust @ minimum throttle, N
-    double rho_2 = 0.8 * T_max; // Thrust @ maximum throttle, N
-    double alpha = 5e-4; // "Mass depletion" constant, s/m
-    double V_max = 90; // Max velocity, m/s
-    double gamma_gs = M_PI / 4; // Lowest glide slope angle, rad
-    double theta = M_PI / 4; // Thrust pointing angle, rad
+    // These values can be found from the first numerical
+    // example from the G-FOLD reference in the README
 
-    Eigen::Vector3d r_0{2400.0, 450.0, -330.0}; // Initial position, m
-    Eigen::Vector3d r_dot_0{-10.0, -40.0, 10.0}; // Initial velocity, m/s
+    gfold::gfold_lander_data lander_data = {
+            .m_0 = 2000,
+            .m_f = 300,
+            .rho_1 = 0.2 * 24000,
+            .rho_2 = 0.8 * 24000,
+            .alpha = 5e-4,
+            .V_max = 90
+    };
 
-    Eigen::Vector2d q{0.0, 0.0}; // Target, m (technically supposed to be in R^2)
-    Eigen::Vector3d g{-3.71, 0.0, 0.0}; // Mars gravitational constant, m/s^2
+    gfold::gfold_trajectory_data trajectory_data = {
+            .r_0 = {2400, 450, -330},
+            .r_dot_0 = {-10, -40, 10},
+            .gamma_gs = M_PI / 4,
+            .theta = M_PI / 4,
+            .n_hat = {1, 0, 0},
+            .q = {0, 0}
+    };
 
-    Eigen::Vector3d omega{2.53e-5, 0.0, 6.62e-5}; // Mars angular velocity, rad/s^2
+    gfold::gfold_planet_data planet_data = {
+            .g = {-3.71, 0, 0},
+            .omega = {2.53e-5, 0.0, 6.62e-5}
+    };
 
-    // TODO: Not fixed a priori
-    double t_f = 60; // Flight t_f, s
-    double dt = 1; // Simulation time step, s
+    gfold::gfold gfold{lander_data, trajectory_data, planet_data, 60, 1};
+    gfold.compute();
 
-    int steps = static_cast<int>(t_f / dt); // Number of steps in the sim
-    int i_f = steps - 1;
+    const Eigen::MatrixXd &x = gfold.get_state_vector();
 
-    Eigen::Vector3d e_1{1, 0, 0};
-    Eigen::Vector3d e_2{0, 1, 0};
-
-    Eigen::Matrix3d S_omega = (Eigen::Matrix3d{} << 0, -omega[2], omega[1],
-            omega[2], 0, -omega[0],
-            -omega[1], omega[0], 0)
-            .finished();
-    Eigen::MatrixXd A = Eigen::MatrixXd::Zero(6, 6);
-    A.topRightCorner<3, 3>() = Eigen::Matrix3d::Identity();
-    A.bottomLeftCorner<3, 3>() = -(S_omega * S_omega);
-    A.bottomRightCorner<3, 3>() = -2 * S_omega;
-
-    Eigen::MatrixXd B = Eigen::MatrixXd::Zero(6, 3);
-    B.bottomLeftCorner<3, 3>() = Eigen::Matrix3d::Identity();
-
-    Eigen::MatrixXd E = (Eigen::Matrix<double, 2, 3>{} << 0, 1, 0,
-            0, 0, 1).finished();
-    Eigen::Vector3d c = e_1 / tan(gamma_gs);
-    Eigen::Vector3d n_hat{1, 0, 0}; // Thrust unit pointing vector
-
-    cvx::OptimizationProblem socp;
-
-    // Add optimization variables
-    const cvx::Scalar &obj = socp.addVariable("obj");
-    const cvx::MatrixX &x = socp.addVariable("x", 6, steps);
-    const cvx::MatrixX &r = x.topRows(3);
-    const cvx::MatrixX &r_dot = x.bottomRows(3);
-    const cvx::MatrixX &m = socp.addVariable("m", 1, steps);
-
-    // 3.1 Change of Variables
-    // const cvx::MatrixX &slack = socp.addVariable("GAMMA", 1, steps);
-    // const cvx::MatrixX &T = socp.addVariable("T", 3, steps);
-    const cvx::MatrixX &sigma = socp.addVariable("sigma", 1, steps);
-    const cvx::MatrixX &u = socp.addVariable("u", 3, steps);
-    const cvx::MatrixX &z = socp.addVariable("z", 1, steps);
-
-    // Set up objective
-    socp.addCostTerm(obj); // Problem 3
-    socp.addConstraint(cvx::lessThan((cvx::par(E) * r.col(i_f) - cvx::par(q)).norm(), obj));
-
-    // Set up constraints
-    socp.addConstraint(cvx::equalTo(m.col(0).sum(), m_0)); // Eq. (7) - Initial mass constraint
-    socp.addConstraint(cvx::box(0, m_0 - m_f, m.col(i_f).sum())); // Eq. (7) - Fuel use constraint
-
-    socp.addConstraint(cvx::equalTo(r.col(0), cvx::par(r_0))); // Eq. (8) - Initial position constraint
-    socp.addConstraint(cvx::equalTo(r_dot.col(0), cvx::par(r_dot_0))); // Eq. (8) - Initial velocity constraint
-
-    socp.addConstraint(
-            cvx::equalTo((cvx::par(e_1).transpose() * r.col(i_f)).sum(), 0)); // Eq. (9) - Final altitude constraint
-    socp.addConstraint(cvx::equalTo(r_dot.col(i_f), 0)); // Eq. (9) - Final velocity constraint
-
-    for (int i = 0; i < steps; ++i) {
-        socp.addConstraint(cvx::lessThan(r_dot.col(i).norm(), cvx::par(V_max))); // Eq. (5) - Velocity constraint
-
-        if (i != i_f) {
-            const cvx::VectorX &diff_r = r.col(i) - r.col(i_f);
-            socp.addConstraint(cvx::lessThan((cvx::par(E) * diff_r).norm() - (cvx::par(c).transpose() * diff_r).sum(),
-                                             0)); // Eq. (5) - Glide slope constraint
-        }
-
-        if (i + 1 <= steps - 1) {
-            // Section 3.1 Change of Variables
-            // cvx::VectorX x_dot = cvx::par(A) * x.col(i) + cvx::par(B) * (cvx::par(g) + (T.col(i) / m.col(i)[0]));
-            // socp.addConstraint(cvx::equalTo(x.col(i) + x_dot * cvx::par(dt), x.col(i + 1))); // Eq. (17)
-            // socp.addConstraint(cvx::equalTo(m.col(i)[0] - cvx::par(alpha) * slack.col(i)[0] * cvx::par(dt), m.col(i + 1)[0])); // Eq. (17)
-
-            cvx::VectorX x_dot = cvx::par(A) * x.col(i) + cvx::par(B) * (cvx::par(g) + u.col(i)); // Eq. (17)
-            socp.addConstraint(cvx::equalTo(x.col(i) + x_dot * cvx::par(dt),
-                                            x.col(i + 1))); // Eq. (17) - System dynamics constraint
-            socp.addConstraint(cvx::equalTo(z.col(i).sum() - cvx::par(alpha) * sigma.col(i).sum(),
-                                            z.col(i + 1))); // Eq. (33) - System mass constraint
-        }
-
-        // Section 3.1 Change of Variables
-        // socp.addConstraint(cvx::lessThan(T.col(i).norm(), slack.col(i)[0])); // Eq. (18)
-        // socp.addConstraint(cvx::box(rho_1, slack.col(i)[0], rho_2)); // Eq. (18)
-        // socp.addConstraint(cvx::greaterThan((cvx::par(n_hat).transpose() * T.col(1)).sum(), cvx::par(cos(theta)) * slack.col(i)[0])); // Eq. (19)
-        socp.addConstraint(cvx::lessThan(u.col(i).norm(),
-                                         sigma.col(i).sum())); // Eq. (34) - Thrust slack constraint
-        socp.addConstraint(cvx::greaterThan(
-                cvx::par(n_hat).transpose() * u.col(i),
-                cvx::par(std::cos(theta)) * sigma.col(i).sum())); // Eq. (34) - Thrust pointing constraint
-
-        double z_0 = std::log(m_0 + m_f - alpha * rho_2 * i * dt);
-        double exp_z_0 = std::exp(z_0);
-        const cvx::Scalar &z_diff = z.col(i).sum() - cvx::par(z_0);
-        // TODO: Squared dynamic var
-        // socp.addConstraint(cvx::box(cvx::par(rho_1 * exp_z_0) * (cvx::par(1) - z_diff + (z_diff * z_diff * cvx::par(2))),
-        // sigma.col(i)[0],
-        // cvx::par(rho_2 * exp_z_0) * (cvx::par(1) - z_diff))); // Eq. (37)
-        socp.addConstraint(cvx::lessThan(
-                sigma.col(i).sum(),
-                cvx::par(rho_2 * exp_z_0) * (cvx::par(1) - z_diff))); // Eq. (36) - Maximum thrust constraint
-    }
-
-    cvx::ecos::ECOSSolver solver{socp};
-    solver.solve(false);
-    Eigen::Vector2d d_P3 = cvx::eval(cvx::par(E) * r.col(i_f));
-    double target = (d_P3 - q).norm();
-
-    // Plotting
     std::vector<double> vec_x;
     std::vector<double> vec_y;
     std::vector<double> vec_z;
 
-    for (int i = 0; i < steps; ++i) {
-        Eigen::Vector3d r_i = cvx::eval(r.col(i));
+    for (int i = 0; i < gfold.get_time_steps(); ++i) {
+        Eigen::Vector3d r_i = x.topRows(3).col(i);
         vec_x.push_back(r_i.x());
         vec_y.push_back(r_i.y());
         vec_z.push_back(r_i.z());
@@ -147,31 +52,7 @@ int main() {
     mpl::xlabel("Y Position (m)");
     mpl::ylabel("Z Position (m)");
     mpl::set_zlabel("X Position (m)");
-    mpl::title("P3: Position vs. Time");
-    mpl::grid(true);
-
-    socp.addCostTerm(sigma.sum()); // Problem 4
-    socp.addConstraint(cvx::lessThan((cvx::par(E) * r.col(i_f) - cvx::par(q)).norm(), cvx::par(target))); // Eq. (20)
-
-    solver.solve(false);
-
-    // Plotting
-    vec_x.clear();
-    vec_y.clear();
-    vec_z.clear();
-
-    for (int i = 0; i < steps; ++i) {
-        Eigen::Vector3d r_i = cvx::eval(r.col(i));
-        vec_x.push_back(r_i.x());
-        vec_y.push_back(r_i.y());
-        vec_z.push_back(r_i.z());
-    }
-
-    mpl::plot3(vec_y, vec_z, vec_x);
-    mpl::xlabel("Y Position (m)");
-    mpl::ylabel("Z Position (m)");
-    mpl::set_zlabel("X Position (m)");
-    mpl::title("P4: Position vs. Time");
+    mpl::title("Position vs. Time");
     mpl::grid(true);
 
     mpl::show();
